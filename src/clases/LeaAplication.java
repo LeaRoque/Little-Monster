a0a3bfdb888b429f7c9cc249918dc5b0e9068a0d
Lea Roque <35973948+LeaRoque@users.noreply.github.com>
2018-02-04 19:17:55 +0000

    Create LeaAplication.java

---
+++ src/clases/LeaAplication.java
@@ -0,0 +1,19 @@
+package clases;
+
+import formularios.frmLogin;
+
+/**
+ *
+ * @author leandraroque
+ */
+public class LeaAplication {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        frmLogin miLogin = new frmLogin();
+        miLogin.setLocationRelativeTo(null);
+        miLogin.setVisible (true);
+    }
+    
+}
